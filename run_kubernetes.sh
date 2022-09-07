c3d5bc63c05f3f51070da845a85266a99ccf4c9a
EC2 Default User <ec2-user@ip-172-31-94-163.ec2.internal>
2022-09-07 17:52:25 +0000

    kubernetes prediction logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="ebunola/operationalizeml"
+dockerpath="seanflutter/microservice"
 
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run operationalizeml\
+kubectl run microservice\
     --image=$dockerpath\
-    --port=80 --labels app=operationalizeml
+    --port=80 --labels app=microservice
 
 # Step 3:
 # List kubernetes pods
@@ -20,4 +20,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward operationalizeml 8000:80
+kubectl port-forward microservice 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,13 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath="ebunola/operationalizeml"
+dockerpath="seanflutter/microservice"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login
-docker tag operationalizeml ${dockerpath}
+docker tag microservice ${dockerpath}
 
 # Step 3:
 # Push image to a docker repository
